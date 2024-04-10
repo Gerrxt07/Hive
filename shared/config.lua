b39ac50b294f998eb594108c436bb963a725668b
Gerrit Schlinkmann <gerrxt07@proton.me>
2024-04-10 18:59:39 +0200

    deleted webhook

--- shared/config.lua
+++ shared/config.lua
@@ -4,4 +4,4 @@ config.version = "1.0.0" -- Do not touch this line too!
 config.language = "de" -- Set the Language for the messages. Available languages: en, de
 config.check_for_updates = true -- Activate this option if you want the system to check for updates automatically.
 
-config.webhook = "https://discord.com/api/webhooks/1191536134061310043/Os0wrryKMmHlPR5euvL56jItkPg6wfnjp_aXEU6Q1ZAK_sP-eZLb6e5iqH21DaZb4c_I" -- Discord-Webhook-URL for logging to a Discord channel. Leave empty to disable.
+config.webhook = "" -- Discord-Webhook-URL for logging to a Discord channel. Leave empty to disable.
