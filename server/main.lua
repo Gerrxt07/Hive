9ad8473056608f2996370bcbe9536e1d44e06153
Gerrit Schlinkmann <gerrxt07@proton.me>
2024-04-11 15:55:19 +0200

    Update

--- server/main.lua
+++ server/main.lua
@@ -1,37 +1,8 @@
-AddEventHandler('playerConnecting', function(err, deferrals)
-    deferrals.defer()
-    deferrals.update("LOCALE: Checking identifier...")
-    Citizen.wait(1000)
-
-    local player = source
-    local identifier = GetPlayerIdentifiers(player)
-
-    local steam = false
-    local discord = false
-
-    for k,v in pairs(identifier) do
-        if v:find("steam:") then
-            steam = true
-        end
-        if v:find("discord:") then
-            discord = true
-        end
-    end
-    if not steam then
-        err("LOCALE: Steam required...")
+AddEventHandler('onResourceStarting', function(resourceName)
+    if resourceName == 'Hive' then
+        print('Name ist nicht Hive')
         CancelEvent()
-        return
     end
-
-    if not discord then
-        err("LOCALE: Discord required...")
-        CancelEvent()
-        return
-    end
-
-    local name = GetPlayerName(player)
-    local ip = GetPlayerEndpoint(player)
-    local tokens = GetPlayerTokens(player)
-
-    deferrals.done("TEST DEATH END")
 end)
+
+AddEventHandler("playerConnecting", PlayerConnecting)
--- shared/functions.lua
+++ shared/functions.lua
@@ -10,3 +10,38 @@ function SendWebhook(webhookURL, message)
     end
     
 end
+
+function PlayerConnecting(name, setKickReason, deferrals)
+    local player = source
+    local identifiers = GetPlayerIdentifiers(player)
+    
+    deferrals.defer()
+    deferrals.update("LOCALE: Checking identifier...")
+    Citizen.wait(1000)
+
+    local steam = false
+    local discord = false
+
+    for _, v in pairs(identifiers) do
+        if v:find("steam:") then
+            steam = true
+        end
+        if v:find("discord:") then
+            discord = true
+        end
+    end
+
+    Citizen.wait(100)
+    
+    if not steam then
+        deferrals.done("LOCALE: Steam required...")
+        return
+    end
+
+    if not discord then
+        deferrals.done("LOCALE: Discord required...")
+        return
+    end
+
+    deferrals.done("TEST DEATH END")
+end
