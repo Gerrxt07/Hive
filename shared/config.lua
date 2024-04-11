501f60e391c16727e97b5c2ad679dbf850df03d2
Gerrit Schlinkmann <gerrxt07@proton.me>
2024-04-11 16:30:41 +0200

    update

--- shared/config.lua
+++ shared/config.lua
@@ -2,6 +2,6 @@ config = {} -- Do not touch this line!
 config.version = "1.0.0" -- Do not touch this line too!
 
 config.language = "de" -- Set the Language for the messages. Available languages: en, de
-config.check_for_updates = true -- Activate this option if you want the system to check for updates automatically.
+config.check_for_updates = false -- Activate this option if you want the system to check for updates automatically.
 
 config.webhook = "https://discord.com/api/webhooks/1227987167914233886/0wfMd6nOunf-z8uuXXe1o749_pw0mRqfdjmF9ypQ7L7XgtqYPiWYuH8qFE3wgGuz3EeC" -- Discord-Webhook-URL for logging to a Discord channel. Leave empty to disable.
