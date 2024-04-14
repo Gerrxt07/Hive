1ef611e244e67c811424b06ce0c70973a27a02f6
Gerrit Schlinkmann <gerrxt07@proton.me>
2024-04-14 17:22:15 +0200

    Edit Server main

--- server/main.lua
+++ server/main.lua
@@ -1,19 +1,24 @@
-AddEventHandler('onResourceStarting', function(resourceName)
-    if resourceName == 'Hive' then
+local ResourceName = "Hive"
+local CurrentResourceName = GetCurrentResourceName()
+
+AddEventHandler('onResourceStarting', function()
+    if CurrentResourceName ~= ResourceName then
         print('Name ist nicht Hive')
         CancelEvent()
     end
 end)
 
 AddEventHandler("onResourceStart", function()
-    UpdateCheck()
+    if CurrentResourceName == ResourceName then
+        UpdateCheck()
 
-    local message = Locales[config.language]['script_started']
-    print(message)
+        local message = Locales[config.language]['script_started']
+        print(message)
 
-    if config.webhook ~= "" then
-        print(Locales[config.language]['webhook_enabled'])
-        return
+        if config.webhook ~= "" then
+            print(Locales[config.language]['webhook_enabled'])
+            return
+        end
     end
 end)
 
